1532fedaaa0a1d438460ac5f655395aed801794b
im-tiket <ikhsan.mahendri@tiket.com>
2024-11-14 18:37:48 +0700

    feat: (setup) - fix build storybook

--- src/components/Form/RadioInput/RadioInput.stories.tsx
+++ src/components/Form/RadioInput/RadioInput.stories.tsx
@@ -1,7 +1,8 @@
 // components/RadioButton/RadioButton.stories.tsx
 import type { Meta, StoryObj } from "@storybook/react";
+import { FormProvider, useForm } from "react-hook-form"; // Import react-hook-form
 
-import RadioButton from ".";
+import RadioButton from "."; // Import your RadioButton component
 
 const meta: Meta<typeof RadioButton> = {
   component: RadioButton,
@@ -11,31 +12,31 @@ const meta: Meta<typeof RadioButton> = {
 };
 
 export default meta;
+
 type Story = StoryObj<typeof RadioButton>;
 
 export const DefaultRadioButton: Story = {
   args: {
     options: [
-      {
-        value: 1,
-        label: "Wash and Fold",
-      },
-      {
-        value: 2,
-        label: "Dry Clean",
-      },
-      {
-        value: 3,
-        label: "Home",
-      },
-      {
-        value: 4,
-        label: "Others",
-      },
+      { value: 1, label: "Wash and Fold" },
+      { value: 2, label: "Dry Clean" },
+      { value: 3, label: "Home" },
+      { value: 4, label: "Others" },
     ],
     name: "radio-group",
-    onChange: (value?: string | number) => {
-      console.log("Selected:", value);
-    },
+  },
+  render: (args) => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const methods = useForm({
+      defaultValues: { radioInput: 1 }, // Set a default value for the radio button (use number for consistency)
+    });
+
+    return (
+      <FormProvider {...methods}>
+        {" "}
+        {/* Wrap your component in FormProvider */}
+        <RadioButton {...args} control={methods.control} /> {/* Pass control from useForm */}
+      </FormProvider>
+    );
   },
 };
--- src/components/Form/RadioInput/RadioInput.types.tsx
+++ src/components/Form/RadioInput/RadioInput.types.tsx
@@ -8,6 +8,6 @@ export type TRadioOption = {
 export interface TRadioButtonProps {
   options?: TRadioOption[]; // The options for the radio buttons
   name: string; // Name for the radio group
-  control: Control<any>;
+  control?: Control<any>;
   onChange?: (value?: string | number) => void; // Callback to send the selected value to parent
 }
